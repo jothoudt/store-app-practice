ef074bc3367648285bf68a9ba2c97812da742d07
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-11-09 21:51:34 -0600

    add layout to product page

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,6 +1,7 @@
 import React,{useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
+import './ProductDetails.css';
 
 function ProductDetails(){
     //define dispatch
@@ -20,7 +21,27 @@ function ProductDetails(){
                 <h2>Loading</h2>
             </div>  :
             <div>
-                <h2>{productDetails[0].product_name}</h2>
+                <div>
+                    <h2>{productDetails[0].product_name}</h2>
+                </div>
+                <div className="product-description-container">
+                    <div>
+                        <img className="product-image" src={productDetails[0].photo} height='500px' width='500px'/>
+                    </div>
+                    <div>
+                        <div className="product-photo-area">
+                            <h4>{productDetails[0].price}</h4>
+                        </div>
+                        <div className="product-information-area">
+                            <div>
+                                <h4>Description:</h4>
+                            </div>
+                            <div>
+                                <p>{productDetails[0].description}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             } 
         </div>
