ba22aa1df76a27330a7ff23cecd3821b8c4b7604
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-11-10 22:44:44 -0600

    Add item to cart on product details page

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -1,16 +1,51 @@
-import React,{useEffect} from 'react';
+import React,{useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
+import AddShoppingCartSharpIcon from '@material-ui/icons/AddShoppingCartSharp';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 import './ProductDetails.css';
 
 function ProductDetails(){
+    //useState
+    let [productQuantity, setProductQuantity]=useState(0);
     //define dispatch
     const dispatch=useDispatch();
     //get id from params
     const id=useParams()
     //get product details from redux store
     const productDetails=useSelector((store)=>{return store.productDetails})
-
+    const user= useSelector((store)=>{return store.user})
+    const addCartItem=()=>{
+        //this will make sure the user has selected a quantity
+        if(productQuantity ===0){
+          return alert('Please select qty')
+        }//end if
+        /*this will check if the user is logged in. If the user is not logged in. They
+        will be alerted to either log in or register. They will not be able to add stuff to their shopping cart
+        without logging in. */
+        if(!user.username){
+          alert('Please Log In or Register')
+        }//end if
+        //object to be sent in dispatch
+        let newOrderItem={
+          product_id: productDetails[0].id,
+          product_name: productDetails[0].product_name,
+          qty: productQuantity,
+          price: productDetails[0].product_price,
+          photo: productDetails[0].photo,
+          user_id: user.id,
+        } //end newOrderItem object
+          console.log('add to cart', newOrderItem)
+          dispatch({type:'ADD_TO_ORDER',payload: newOrderItem})
+          alert('Item Added to Cart')
+      } //end addCartItem
+    //this function changes the quantity of the item wanted
+    const handleChange=(event)=>{
+    setProductQuantity(event.target.value);
+    } //end handleChange
     useEffect(()=>
         dispatch({type:'FETCH_PRODUCT_DETAILS',payload:id})
     ,[])
@@ -38,6 +73,23 @@ function ProductDetails(){
                             </div>
                             <div>
                                 <p>{productDetails[0].description}</p>
+                                <AddShoppingCartSharpIcon style={{marginTop: "auto", marginRight: "24px", fontSize:"48px", color:"#48BF84"}} onClick={addCartItem} />
+                                <FormControl variant="outlined" >
+                                <InputLabel id="demo-simple-select-outlined-label">Quantity</InputLabel>
+                                    <Select
+                                        style={{height:"50px", width:"120px"}}
+                                        labelId="demo-simple-select-outlined-label"
+                                        id="demo-simple-select-outlined"
+                                        value={productQuantity}
+                                        onChange={handleChange}
+                                    >
+                                        <MenuItem value={1}>1</MenuItem>
+                                        <MenuItem value={2}>2</MenuItem>
+                                        <MenuItem value={3}>3</MenuItem>
+                                        <MenuItem value={4}>4</MenuItem>
+                                        <MenuItem value={5}>5</MenuItem>
+                                    </Select>
+                                </FormControl>
                             </div>
                         </div>
                     </div>
